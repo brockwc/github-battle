fdf2a545045f336ca2561be50c912554c9cc5011
brockwc <brock.whitbread@gmail.com>
2018-11-16 16:19:44 -0800

    refactor round 2; everything working now; remember to look out for implicit vs. explicit returns

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,27 +1,22 @@
-import axios from 'axios';
+const axios = require('axios');
 
-const token = "787b12207b67d52ebedcfa817bca4413b231de6b"
+const token = "584b249b41194cd224fa44825db04a8c37aebee2"
 const params = `?access_token=${token}`
 
 const getProfile = (username) => {
-  axios.get(`https://api.github.com/users/${ username }${ params }`)
-    .then(user => user.data);
+  return axios.get(`https://api.github.com/users/${ username }${ params }`)
+    .then(({ data }) => console.log(data) || data);
 }
 
 const getRepos = (username) => {
-  axios.get(`https://api.github.com/users/${ username }/repos${ params }`);
+  return axios.get(`https://api.github.com/users/${ username }/repos${ params }`);
 }
 
 const getStarCount = (repos) => {
-  repos.data.reduce((count, repo) => { count + repo.stargazers_count }, 0);
+  return repos.data.reduce((count, { stargazers_count }) => count + stargazers_count, 0);
 }
 
-const calculateScore = (profile, repos) => {
-  const followers = profile.followers;
-  const totalStars = getStarCount(repos);
-
-  return (followers * 3) + totalStars;
-}
+const calculateScore = ({ followers }, repos) => (followers * 3) + getStarCount(repos);
 
 const handleError = (error) => {
   console.warn(`An error occured: ${error}`);
@@ -29,36 +24,29 @@ const handleError = (error) => {
 }
 
 const getUserData = (player) => {
-  axios.all([
+  return Promise.all([
     getProfile(player),
     getRepos(player)
-  ]).then((data) => {
-    let profile = data[0];
-    let repos = data[1];
-
-    return {
-      profile,
-      score: calculateScore(profile, repos)
-    }
-  })
+  ]).then(([profile, repos]) => console.log([profile, repos]) || ({
+    profile,
+    score: calculateScore(profile, repos)
+  }));
 }
 
 const sortPlayers = (players) => {
-  players.sort((a, b) => b.score - a.score);
+  return players.sort((a, b) => b.score - a.score);
 }
 
-export default {
-  battle: function (players) {
-    return axios.all(players.map(getUserData))
+module.exports = {
+  battle (players) {
+    return Promise.all(players.map(getUserData))
       .then(sortPlayers)
       .catch(handleError);
   },
-  fetchPopularRepos: function (language) {
-    const encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories')
+  fetchPopularRepos (language) {
+    const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${ language }&sort=stars&order=desc&type=Repositories`)
 
     return axios.get(encodedURI)
-      .then(function (response) {
-        return response.data.items;
-      })
+      .then(({ data }) => data.items);
   }
 }
